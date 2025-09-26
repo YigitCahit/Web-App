d68b2c7cffea60c6cb5b4c797170501665c42d51
YigitCahit <yigitmertcahit@gmail.com>
2025-09-26 16:07:40 +0300

    Add post update and delete functionality with authorization checks.

--- app/app.py
+++ app/app.py
@@ -59,6 +59,37 @@ def new_post():
         return redirect(url_for('main.home'))
     return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
+@main.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash('You are not authorized to edit this post.', 'danger')
+        return redirect(url_for('main.home'))
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('main.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+@main.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash('You are not authorized to delete this post.', 'danger')
+        return redirect(url_for('main.home'))
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
+
 @main.route('/post/<int:post_id>')
 def post(post_id):
     post = Post.query.get_or_404(post_id)
